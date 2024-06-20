722bbfb512a0b00aadfebed33362075104d0c5a3
anoop <anoopsabu@live.com>
2024-06-20 11:02:19 +0530

    allowed host added

--- gearup_garage/settings.py
+++ gearup_garage/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.60.63.222', 'www.gearupgarage.heyanoop.online']
 
 
 # Application definition
